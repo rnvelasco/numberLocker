b92b05d2d81e260a4c6ce22ec6c43a6aacd047a7
Robert Velasco <robertvelasco@Roberts-MacBook-Pro.local>
2022-10-25 19:19:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function CalcDisplay({display}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calcDisplay">
+      {display}
     </div>
+  );
+}
+
+function CalcButton({value, onClick}) {
+  return (
+    <button className="calcButton" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+function App() {
+  
+  const [num, setNum] = useState(0);
+  const [disp, setDisp] = useState("ENTER CODE");
+
+  const numberClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+      if (num === 0) {
+        setNum(value)
+        setDisp(value);
+      } else {
+        setNum(num+value)
+        setDisp(num+value);
+      }
+    console.log(num.length);
+    console.log(num +'|'+ disp);
+  };
+
+  const enterClickHandler = () => {    
+    if(num == "1234567890") {
+      setDisp("OPEN");
+    } else {
+      setDisp("LOCKED");
+    }
+
+    setNum(0);
+    console.log(num +'|'+ disp);    
+  };
+
+  const clearClickHandler = () => {
+    setNum(0);
+    setDisp("ENTER CODE");
+
+    console.log(num +'|'+ disp);
+  };
+
+  return ( 
+    <div className="calcContainer">
+      <CalcDisplay display={disp}/>
+      <div className="calcButtonsContainer">
+        <CalcButton value="7" onClick={numberClickHandler}/>
+        <CalcButton value="8" onClick={numberClickHandler}/>
+        <CalcButton value="9" onClick={numberClickHandler}/>
+        <CalcButton value="4" onClick={numberClickHandler}/>
+        <CalcButton value="5" onClick={numberClickHandler}/>
+        <CalcButton value="6" onClick={numberClickHandler}/>
+        <CalcButton value="1" onClick={numberClickHandler}/>
+        <CalcButton value="2" onClick={numberClickHandler}/>
+        <CalcButton value="3" onClick={numberClickHandler}/>
+        <CalcButton value="Reset" onClick={clearClickHandler}/>
+        <CalcButton value="0" onClick={numberClickHandler}/>
+        <CalcButton value="Enter" onClick={enterClickHandler}/>
+      </div>  
+    </div>  
   );
 }
 
